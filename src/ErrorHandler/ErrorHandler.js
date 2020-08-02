63e04031795820d13f0445baa9e7e9f5156e43bd
Giggs <gigasii2856@gmail.com>
2020-08-02 17:31:21 +0800

    Error handling

---
+++ src/ErrorHandler/ErrorHandler.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class ErrorHandler extends Component {
+    state = {
+        hasError: false,
+        errorMessage: ''
+	}
+	
+	componentDidCatch = (error, info) => {
+		this.setState({
+			hasError: true,
+			errorMessage: error
+		})
+	}
+
+	render()
+	{
+		return this.state.hasError ? <h1>{this.state.errorMessage}</h1> : this.props.children;
+	}
+}
