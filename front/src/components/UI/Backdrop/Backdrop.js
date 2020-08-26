f98ff3df2c7699ca719eaebd0dde10269aa38407
Giggs <gigasii2856@gmail.com>
2020-08-26 22:23:33 +0800

    Deploy for backend

--- front/src/components/UI/Backdrop/Backdrop.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Class from './Backdrop.css';
-
-const backdrop = (props) => (
-   props.show ? <div className={Class.Backdrop} onClick={props.clicked}/> : null
-);
-
-export default backdrop;
--- front/src/components/UI/Spinner/Spinner.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Class from './Spinner.css';
-
-const spinner = () => (
-    <div className={Class.Loader}>Loading...</div>
-);
-
-export default spinner;
